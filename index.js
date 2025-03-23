7deb209369ea324465ece9fd0589602b185990f6
Harshul040 <150358591+Harshul040@users.noreply.github.com>
2025-03-23 23:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const { connection } = require("./connect");
 const urlRoute = require("./routes/url");
 
 const app = express();
 const PORT = 8001;
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/url", urlRoute);
 
@@ -13,7 +15,7 @@ app.get('/:shortId', async (req, res) => {
 
     try {
         
-        const result = await connection.query("SELECT * FROM url_shortner.urls WHERE shortid = $1", [shortId]);//change
+        const result = await connection.query("SELECT * FROM url_shortner.urls WHERE shortid = $1", [shortId]);
         if (result.rows.length === 0) {
             return res.status(404).json({ error: "Short URL not found" });
         }
@@ -29,4 +31,37 @@ app.get('/:shortId', async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+// const express = require("express");
+// const { connection } = require("./connect");
+// const urlRoute = require("./routes/url");
+
+// const app = express();
+// const PORT = 8001;
+
+// app.use(express.json());
+// app.use("/url", urlRoute);
+
+// app.get('/:shortId', async (req, res) => {
+//     const shortId = req.params.shortId;
+
+//     try {
+        
+//         const result = await connection.query("SELECT * FROM url_shortner.urls WHERE shortid = $1", [shortId]);//change
+//         if (result.rows.length === 0) {
+//             return res.status(404).json({ error: "Short URL not found" });
+//         }
+//         const entry = result.rows[0];
+
+//         await connection.query("UPDATE url_shortner.urls SET visitcount = visitcount + 1 WHERE shortid = $1", [shortId]);
+
+//         res.redirect(entry.redirecturl);
+//     } catch (err) {
+//         console.error("Error fetching URL:", err);
+//         res.status(500).json({ error: "Database error 1" });
+//     }
+// });
+
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
